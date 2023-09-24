477b8123b1e0e216b8128192ff24ca147e5414d9
blessdog <waterbreathers@gmail.com>
2023-09-23 20:16:03 -0600

    flipper

---
+++ part 3/app.js
@@ -0,0 +1,6 @@
+// first step: grabbing element to change
+let title = document.querySelector("title");
+let paragraph1 = document.querySelector("first-paragraph");
+let paragraph2 = document.querySelector("second-paragraph");
+
+// second step:
