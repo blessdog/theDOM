4094afcf5b2cfba8f75608c45c1990067590c96f
blessdog <waterbreathers@gmail.com>
2023-09-24 20:20:21 -0600

    sundsay sept24

--- part 3/app.js
+++ part 3/app.js
@@ -4,3 +4,9 @@ let paragraph1 = document.querySelector("first-paragraph");
 let paragraph2 = document.querySelector("second-paragraph");
 
 // second step:
+function blue() {
+  let color = "blue";
+  title.setAttribute("style", "color: " + color);
+  paragraph1.setAttribute("style", "color: " + color);
+  paragraph2.setAttribute("style", "color: ${color}");
+}
