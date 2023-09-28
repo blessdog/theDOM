82d1f9c13173f99aefb0e3c6b61c02ed6ea01c70
blessdog <waterbreathers@gmail.com>
2023-09-28 15:39:37 -0600

    passing function to other functions

--- part3/app.js
+++ part3/app.js
@@ -1,12 +1,19 @@
 // first step: grabbing element to change
-let title = document.querySelector("title");
-let paragraph1 = document.querySelector("first-paragraph");
-let paragraph2 = document.querySelector("second-paragraph");
+let title = document.getElementById("title");
+let paragraph1 = document.getElementById("first-paragraph");
+let paragraph2 = document.getElementById("second-paragraph");
 
 // second step:
 function blue() {
   let color = "blue";
   title.setAttribute("style", "color: " + color);
-  paragraph1.setAttribute("style", "color: " + color);
-  paragraph2.setAttribute("style", "color: ${color}");
+  paragraph1.setAttribute("style", `color: ${color}`);
+  paragraph2.setAttribute("style", `color: ${color}`);
+}
+// third step:
+function red() {
+  let color = "red";
+  title.setAttribute("style", "color: " + color);
+  paragraph1.setAttribute("style", `color: ${color}`);
+  paragraph2.setAttribute("style", `color: ${color}`);
 }
